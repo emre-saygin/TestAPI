ea78b57280ff84976ae941a0a547e8c46fe97baf
Emre SAYGIN <emresaygin@hotmail.com.tr>
2025-08-31 01:37:50 +0300

    dockerfile dosyası değiştirildi.

--- TestAPI/obj/Debug/net8.0/TestAPI.AssemblyInfo.cs
+++ TestAPI/obj/Debug/net8.0/TestAPI.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f73872f160da9811cf7af5c81490c1975ddb850")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

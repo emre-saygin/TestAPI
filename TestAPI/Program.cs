130dda3adfd222afb6bc6958c4fa4eab447a2938
Emre SAYGIN <emresaygin@hotmail.com.tr>
2025-08-31 02:24:55 +0300

    port değiştirildi.

--- TestAPI/Program.cs
+++ TestAPI/Program.cs
@@ -3,7 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.WebHost.ConfigureKestrel(options =>
 {
-    options.ListenAnyIP(5000); // HTTP
+    options.ListenAnyIP(80); // HTTP
     // options.ListenAnyIP(5001, listenOptions => listenOptions.UseHttps()); // HTTPS’i devre dýþý býrak
 });
 // Add services to the container.
